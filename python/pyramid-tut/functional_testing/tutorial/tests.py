32449f4701d756f757097359fb819587a30fa926
d9k <d9k@ya.ru>
2015-06-28 00:19:22 +0300

    pyramid tut 06: Functional Testing with WebTest

---
+++ python/pyramid-tut/functional_testing/tutorial/tests.py
@@ -0,0 +1,31 @@
+import unittest
+
+from pyramid import testing
+
+# execute `nosetests tutorial`
+
+class TutorialViewTests(unittest.TestCase):
+    def setUp(self):
+        self.config = testing.setUp()
+
+    def tearDown(self):
+        testing.tearDown()
+
+    def test_hello_world(self):
+        from tutorial import hello_world
+
+        request = testing.DummyRequest()
+        responce = hello_world(request)
+        self.assertEqual(responce.status_code, 200)
+
+class TutorialFunctionalTests(unittest.TestCase):
+    def setUp(self):
+        from tutorial import main
+        app = main({})
+        from webtest import TestApp
+
+        self.testApp = TestApp(app)
+
+    def test_hello_world(self):
+        res = self.testApp.get('/', status=200)
+        self.assertIn(b'<h1>Hello World!</h1>', res.body)
