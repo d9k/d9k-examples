0d2be1bec03ead60ee4f6360ca43cc9dde264a12
d9k <d9k@ya.ru>
2018-04-18 15:04:25 +0300

    mithril playground with three counters. mithril rerenders everything on global store change!

--- js/mithril/counter.js
+++ js/mithril/counter.js
@@ -5,37 +5,55 @@ docReady(function() {
     //window.data = {};
     //data.count = 0;
 
-    storeSet('count', 0)
+    storeSet('count1', 0)
+    storeSet('count2', 0)
 
-    var display = document.getElementById('display');
+    var elById = document.getElementById.bind(document);
 
     var Hello = {
         oninit: function(vnode) {
             this.count_path = vnode.attrs.count_path;
+            this.delta = vnode.attrs.delta || 1;
             //this.store = vnode.attrs.store;
         },
         view: function(vnode) {
+            console.log(JSON.stringify(vnode.attrs) + ' rerender');
+
             var self = this;
 
-            return m("main", [
-                m("h1", {class: "title"}, "My first app"),
+            //m("main", [
+                //m("h1", {class: "title"}, "My first app"),
 
                 // changed the next line
-                m("button", {
+
+            return m("button", {
                     onclick: function() {
                       var count = storeGet(self.count_path);
-                      storeSet(self.count_path, count+1);
+                      storeSet(self.count_path, count + self.delta);
                     }
                   },
-                  storeGet(self.count_path) + " clicks"
-                ),
-            ])
+                  self.count_path + ': ' + storeGet(self.count_path)
+                );//,
+            //)
         }
     }
 
-    m.mount(display, {view: function () {
+    m.mount(elById('counter1'), {view: function () {
       return m(Hello, {
-        count_path: 'count'/*, store: window._store*/
+        count_path: 'count1'
+      })
+    }});
+
+    m.mount(elById('counter1_10'), {view: function () {
+      return m(Hello, {
+        count_path: 'count1',
+        delta: 10,
+      })
+    }});
+
+    m.mount(elById('counter2'), {view: function () {
+      return m(Hello, {
+        count_path: 'count2'
       })
     }});
 });
