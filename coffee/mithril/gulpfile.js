2af1f7803df87e6be2e7e84da4b4edbf19fb8bd9
d9k <d9k@ya.ru>
2018-05-11 10:29:02 +0300

    mithril+coffee: direct transpilation; getting read of babel gulp task

--- coffee/mithril/gulpfile.js
+++ coffee/mithril/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp    = require('gulp'),
     coffee  = require('gulp-coffee'),
     babel   = require('gulp-babel'),
+    sourcemaps = require('gulp-sourcemaps'),
     gutil   = require('gulp-util');
 
 config = {
@@ -13,35 +14,53 @@ gulp.task('coffee', function(){
     gulp.src(config.coffeePath + '/**/*.coffee')
     //gulp.src(config.coffeePath + '/*.coffee')
         // bare option: see http://coffeescript.org/#lexical-scope
-        .pipe(coffee({bare: true}))//.on('error', gutil.log))
-        .pipe(gulp.dest(config.es6Path));
+        //.pipe(sourcemaps.init())
+        .pipe(coffee({
+          transpile: {
+            presets: [
+              // Without any configuration options, babel-preset-env behaves exactly the same as babel-preset-latest (or babel-preset-es2015, babel-preset-es2016, and babel-preset-es2017 together).
+              'env',
+            ],
+            plugins: [
+              ["transform-react-jsx", {
+                "pragma": "m" // default pragma is React.createElement
+                //,useBuiltIns: true
+              }]
+            ],
+          }
+        }))//.on('error', gutil.log))
+        //.pipe(coffee({bare: true}))//.on('error', gutil.log))
+        //.pipe(sourcemaps.write())
+        //.pipe(gulp.dest(config.es6Path));
+        .pipe(gulp.dest(config.staticDir + '/js'));
 });
 
-gulp.task('es6', function () {
-	gulp.src(
-    config.es6Path + '/**/*.js'
-  )
-	.pipe(babel({
-		presets: [
-      // Without any configuration options, babel-preset-env behaves exactly the same as babel-preset-latest (or babel-preset-es2015, babel-preset-es2016, and babel-preset-es2017 together).
-      'env',
+//gulp.task('es6', function () {
+//	gulp.src(
+//    config.es6Path + '/**/*.js'
+//  )
+//	.pipe(babel({
+//		presets: [
+//      // Without any configuration options, babel-preset-env behaves exactly the same as babel-preset-latest (or babel-preset-es2015, babel-preset-es2016, and babel-preset-es2017 together).
+//      'env',
+//
+//    //'es2015', 'stage-0'//, 'react'
+//    //, 'es2016', 'es2017'
+//    ],
+//    plugins: [
+//      ["transform-react-jsx", {
+//        "pragma": "m" // default pragma is React.createElement
+//        //,useBuiltIns: true
+//      }]
+//    ],
+//    // plugins: ['transform-runtime'],
+//		//sourceRoot: 'src',
+////    sourceRoot: src.babelSourceRoot,
+//		//sourceMap: true
+//	}))
+//	.pipe(gulp.dest(config.staticDir + '/js'));
+//});
 
-    //'es2015', 'stage-0'//, 'react'
-    //, 'es2016', 'es2017'
-    ],
-    plugins: [
-      ["transform-react-jsx", {
-        "pragma": "m" // default pragma is React.createElement
-        //,useBuiltIns: true
-      }]
-    ],
-    // plugins: ['transform-runtime'],
-		sourceRoot: 'src',
-//    sourceRoot: src.babelSourceRoot,
-		sourceMap: true
-	}))
-	.pipe(gulp.dest(config.staticDir + '/js'));
-});
 
-
-gulp.task('default', ['coffee', 'es6']);
+//gulp.task('default', ['coffee', 'es6']);
+gulp.task('default', ['coffee']);
--- coffee/mithril/js/store.js
+++ coffee/mithril/js/store.js
@@ -1,136 +1,138 @@
 'use strict';
 
-//function explode( delimiter, string ) {	// Split a string by string
-////
-//	 //+   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
-//	 //+   improved by: kenneth
-//	 //+   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+(function () {
+  //function explode( delimiter, string ) {	// Split a string by string
+  ////
+  //	 //+   original by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
+  //	 //+   improved by: kenneth
+  //	 //+   improved by: Kevin van Zonneveld (http://kevin.vanzonneveld.net)
 
-//var emptyArray = { 0: '' };
+  //var emptyArray = { 0: '' };
 
-//if ( arguments.length != 2
-//|| typeof arguments[0] == 'undefined'
-//|| typeof arguments[1] == 'undefined' )
-//{
-//return null;
-//}
+  //if ( arguments.length != 2
+  //|| typeof arguments[0] == 'undefined'
+  //|| typeof arguments[1] == 'undefined' )
+  //{
+  //return null;
+  //}
 
-//if ( delimiter === ''
-//|| delimiter === false
-//|| delimiter === null )
-//{
-//return false;
-//}
+  //if ( delimiter === ''
+  //|| delimiter === false
+  //|| delimiter === null )
+  //{
+  //return false;
+  //}
 
-//if ( typeof delimiter == 'function'
-//|| typeof delimiter == 'object'
-//|| typeof string == 'function'
-//|| typeof string == 'object' )
-//{
-//return emptyArray;
-//}
+  //if ( typeof delimiter == 'function'
+  //|| typeof delimiter == 'object'
+  //|| typeof string == 'function'
+  //|| typeof string == 'object' )
+  //{
+  //return emptyArray;
+  //}
 
-//if ( delimiter === true ) {
-//delimiter = '1';
-//}
+  //if ( delimiter === true ) {
+  //delimiter = '1';
+  //}
 
-//return string.toString().split ( delimiter.toString() );
-//}
-//function storeNodeExists(path) {
-//var pathParts = path.split('.');
-//var curentPointer = window._store;
-//for (var i = 0; i < pathParts.length; i++) {
-//var pathPart = pathParts[i];
-
-////console.log();
-//if curentPointer.hasOwnProperty(pathPart) {
-//curentPointer = curentPointer[pathPart];
-//} else {
-//return false;
-//}
-//}
-//return true;
-//}
-var localStoreGet, localStoreSet, storeGet, storeSet;
-
-localStoreGet = function localStoreGet(store, path) {
-  var curentPointer, i, pathPart, pathParts;
-  pathParts = path.split('.');
+  //return string.toString().split ( delimiter.toString() );
+  //}
+  //function storeNodeExists(path) {
+  //var pathParts = path.split('.');
   //var curentPointer = window._store;
-  curentPointer = store;
-  i = 0;
-  while (i < pathParts.length) {
-    pathPart = pathParts[i];
-    if (!curentPointer.hasOwnProperty(pathPart)) {
-      return void 0;
-    }
-    curentPointer = curentPointer[pathPart];
-    i++;
-  }
-  return curentPointer;
-};
+  //for (var i = 0; i < pathParts.length; i++) {
+  //var pathPart = pathParts[i];
 
-localStoreSet = function localStoreSet(store, path, value) {
-  var curentPointer, i, pathPart, pathParts;
-  pathParts = path.split('.');
-  //var curentPointer = window._store;
-  curentPointer = store;
-  i = 0;
-  while (i < pathParts.length) {
-    pathPart = pathParts[i];
-    // console.log();
-    if (i < pathParts.length - 1) {
+  ////console.log();
+  //if curentPointer.hasOwnProperty(pathPart) {
+  //curentPointer = curentPointer[pathPart];
+  //} else {
+  //return false;
+  //}
+  //}
+  //return true;
+  //}
+  var localStoreGet, localStoreSet, storeGet, storeSet;
+
+  localStoreGet = function localStoreGet(store, path) {
+    var curentPointer, i, pathPart, pathParts;
+    pathParts = path.split('.');
+    //var curentPointer = window._store;
+    curentPointer = store;
+    i = 0;
+    while (i < pathParts.length) {
+      pathPart = pathParts[i];
       if (!curentPointer.hasOwnProperty(pathPart)) {
-        curentPointer[pathPart] = {};
+        return void 0;
       }
       curentPointer = curentPointer[pathPart];
-    } else {
-      curentPointer[pathPart] = value;
+      i++;
     }
-    i++;
-  }
-};
+    return curentPointer;
+  };
 
-storeGet = function storeGet(path) {
-  //  var pathParts = path.split('.');
-  //  var curentPointer = window._store;
+  localStoreSet = function localStoreSet(store, path, value) {
+    var curentPointer, i, pathPart, pathParts;
+    pathParts = path.split('.');
+    //var curentPointer = window._store;
+    curentPointer = store;
+    i = 0;
+    while (i < pathParts.length) {
+      pathPart = pathParts[i];
+      // console.log();
+      if (i < pathParts.length - 1) {
+        if (!curentPointer.hasOwnProperty(pathPart)) {
+          curentPointer[pathPart] = {};
+        }
+        curentPointer = curentPointer[pathPart];
+      } else {
+        curentPointer[pathPart] = value;
+      }
+      i++;
+    }
+  };
 
-  //  for (var i = 0; i < pathParts.length; i++) {
-  //    var pathPart = pathParts[i];
+  storeGet = function storeGet(path) {
+    //  var pathParts = path.split('.');
+    //  var curentPointer = window._store;
 
-  //    // console.log();
-  //    if (!curentPointer.hasOwnProperty(pathPart)) {
-  //      return undefined;
-  //    }
-  //    curentPointer = curentPointer[pathPart];
-  //  }
-  //  return curentPointer;
-  return localStoreGet(window._store, path);
-};
+    //  for (var i = 0; i < pathParts.length; i++) {
+    //    var pathPart = pathParts[i];
 
-storeSet = function storeSet(path, value) {
-  //  var pathParts = path.split('.');
-  //  var curentPointer = window._store;
+    //    // console.log();
+    //    if (!curentPointer.hasOwnProperty(pathPart)) {
+    //      return undefined;
+    //    }
+    //    curentPointer = curentPointer[pathPart];
+    //  }
+    //  return curentPointer;
+    return localStoreGet(window._store, path);
+  };
 
-  //  for (var i = 0; i < pathParts.length; i++) {
-  //    var pathPart = pathParts[i];
+  storeSet = function storeSet(path, value) {
+    //  var pathParts = path.split('.');
+    //  var curentPointer = window._store;
 
-  //    // console.log();
-  //    if (i < pathParts.length - 1) {
-  //      if (!curentPointer.hasOwnProperty(pathPart)) {
-  //        curentPointer[pathPart] = {};
-  //      }
-  //      curentPointer = curentPointer[pathPart];
-  //    } else {
-  //      curentPointer[pathPart] = value;
-  //    }
-  //  }
-  // if value is object, assign recursively, see https://github.com/jonschlinkert/assign-deep
-  //curentPointer = value;
-  return localStoreSet(window._store, path, value);
-};
+    //  for (var i = 0; i < pathParts.length; i++) {
+    //    var pathPart = pathParts[i];
 
-window._store = {};
+    //    // console.log();
+    //    if (i < pathParts.length - 1) {
+    //      if (!curentPointer.hasOwnProperty(pathPart)) {
+    //        curentPointer[pathPart] = {};
+    //      }
+    //      curentPointer = curentPointer[pathPart];
+    //    } else {
+    //      curentPointer[pathPart] = value;
+    //    }
+    //  }
+    // if value is object, assign recursively, see https://github.com/jonschlinkert/assign-deep
+    //curentPointer = value;
+    return localStoreSet(window._store, path, value);
+  };
 
-// ---
-// generated by js2coffee 2.2.0
+  window._store = {};
+
+  // ---
+  // generated by js2coffee 2.2.0
+}).call(undefined);
