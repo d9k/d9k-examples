6634570c08a2fb50eac720026ea62f84d41345f6
d9k <d9k@ya.ru>
2023-12-21 19:57:25 +0500

    PSQL: arrays+: from SELECT

--- psql/arrays.sql
+++ psql/arrays.sql
@@ -18,3 +18,19 @@ from
   (
     select array['apple', 'banana', 'feijoa'] as ar
   ) as array_declaration;
+
+
+WITH temp(val, t) AS ( VALUES
+  (1, 'A'),
+  (3, 'B'),
+  (2, 'C'),
+  (4, 'B'),
+  (5, 'A')
+),
+temp2 AS (
+  SELECT ARRAY(SELECT (t) from temp) AS ar
+)
+SELECT *
+  -- NOT IN PSQL!
+  -- , ar_distinct AS ARRAY_DISTINCT(ar)
+FROM temp2;
